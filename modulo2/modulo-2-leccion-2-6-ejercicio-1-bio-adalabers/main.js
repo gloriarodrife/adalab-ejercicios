accabc914a16db5d98ccd7644e23b28ee663cc4c
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-03-24 13:17:26 +0100

    exercises with objects

---
+++ modulo2/modulo-2-leccion-2-6-ejercicio-1-bio-adalabers/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const adalaber1 = {
+  name: 'Susana',
+  age: 34,
+  profesion: ' periodista',
+};
+
+console.log(
+  `Mi nombre es ${adalaber1.name}, tengo ${adalaber1.age} y soy ${adalaber1.profesion}`,
+);
+
+const adalaber2 = {
+  name: 'Rocío',
+  age: 25,
+  profesion: ' actriz',
+};
+
+console.log(`${adalaber2.name}, ${adalaber2.age} años, ${adalaber2.profesion}`);
