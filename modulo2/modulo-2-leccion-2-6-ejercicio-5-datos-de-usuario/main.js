e27e4043d7b3b89e52f59efe30aba3263b612515
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-03-24 19:43:36 +0100

    objects

---
+++ modulo2/modulo-2-leccion-2-6-ejercicio-5-datos-de-usuario/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const user = {
+  job: 'developer',
+  firstName: 'Abdon',
+  lastName: 'Davila',
+  age: 30,
+};
+
+user.age += 1;
+console.log(user);
+user.firstName = 'Roberto';
+console.log(user);
+console.log(user);
