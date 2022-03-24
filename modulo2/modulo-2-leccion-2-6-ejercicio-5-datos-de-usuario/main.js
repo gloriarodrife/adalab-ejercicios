231cfd50008e2d57372240b5ac2e08f0dfa711da
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-03-24 23:42:49 +0100

    modifying objets

--- modulo2/modulo-2-leccion-2-6-ejercicio-5-datos-de-usuario/main.js
+++ modulo2/modulo-2-leccion-2-6-ejercicio-5-datos-de-usuario/main.js
@@ -8,7 +8,28 @@ const user = {
 };
 
 user.age += 1;
+user.age++;
 console.log(user);
 user.firstName = 'Roberto';
 console.log(user);
 console.log(user);
+
+//
+
+const adalaber = {
+  name: 'Gloria',
+  sayHello: function () {
+    return `Hola, me llamo ${this.name}`;
+  },
+};
+
+console.log(adalaber.sayHello());
+
+// Esto es una referencia al objeto inicial. NO copia el objeto
+const abdon = adalaber;
+// Por lo tanto, aquí modifico el name del objeto adalaber
+abdon.name = 'Abdón';
+
+console.log(abdon.sayHello());
+
+console.log(adalaber.sayHello());
