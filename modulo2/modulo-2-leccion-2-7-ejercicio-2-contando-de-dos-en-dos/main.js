c7550323d67eb2cf004716a4962bf6295aeb8ea1
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-03-25 19:37:48 +0100

    fix exercise

--- modulo2/modulo-2-leccion-2-7-ejercicio-2-contando-de-dos-en-dos/main.js
+++ modulo2/modulo-2-leccion-2-7-ejercicio-2-contando-de-dos-en-dos/main.js
@@ -1,7 +1,10 @@
 'use strict';
 
-const acc = 0;
-
-for (let acc = 2; acc < 21; acc++) {
-  console.log(`El resultado es: ${acc++}`);
+// for (let acc = 2; acc < 21; acc++) {
+//   console.log(`El resultado es: ${acc++}`);
+// }
+let acc = 0;
+for (let index = 0; index < 10; index++) {
+  acc = acc + 2;
 }
+console.log(`El resultado es: ${acc}`);
