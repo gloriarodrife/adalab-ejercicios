4df72f5a464c852b27ea106a813102b73e06b81b
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-03-27 14:31:18 +0200

    buttons click event andd add classList

---
+++ modulo2/modulo-2-leccion-2-7-ejercicio-2-botones-de-alarma/main.js
@@ -0,0 +1,11 @@
+// Vamos a partir de un HTML que tiene 3 botones con el texto ALARMA en un fondo blanco. Vamos a hacer que al pulsar en cualquiera de ellos, el fondo de la pantalla se ponga rojo. Si volvemos a pulsar en cualquiera de ellos, el fondo se pondrá blanco. Y así sucesivamente. Vamos a hacer uso de querySelectorAll para evitar repetir mucho código.
+
+'use strict';
+
+const buttons = document.querySelectorAll('.js-button');
+
+for (const button of buttons) {
+  button.addEventListener('click', () => {
+    button.classList.toggle('color');
+  });
+}
