dc555d928a4648c72cfb3113bfd52afb70b19b4b
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-03-28 00:04:55 +0200

    data types of an array

---
+++ modulo2/modulo-2-leccion-2-7-ejercicio-4-datos-de-un-array/main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const items = [
+  'Ada',
+  1815,
+  ['Informática', 'Matemática', 'Escritora'],
+  {
+    mother: 'Anna Isabella',
+    father: 'Lord Byron',
+  },
+];
+
+function iterator(array) {
+  for (let index = 0; index < array.length; index++) {
+    let item = array[index];
+    let type = typeof item;
+
+    // Comprobamos si un array y pintamos el tipo array y no object
+    if (Array.isArray(item)) {
+      type = 'Array';
+    }
+    // Si es tipo object lo conbertimos en un string con el metodo JSON.stringify
+    if (type === 'object') {
+      item = JSON.stringify(item);
+    }
+
+    const message = `El dato ${item} está en la posición ${index} y es de tipo ${type}`;
+
+    console.log(message);
+  }
+
+  // for (const [index, item] of items.entries()) {
+  //   console.log();
+  // }
+}
+
+const position = iterator(items);
