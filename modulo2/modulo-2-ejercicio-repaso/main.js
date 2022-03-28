39b1eb989e00531944fe036b30afd9652882f68e
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-03-28 13:13:52 +0200

    review exercise with random numbers, events and functions

---
+++ modulo2/modulo-2-ejercicio-repaso/main.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const text = document.querySelector('.js-text');
+const update = document.querySelector('.js-update');
+const body = document.querySelector('.js-body');
+const face = document.querySelector('.js-face');
+
+// Generar un número aleatorio
+
+function GenerateRandomNumb(max) {
+  let randomNumb = parseInt(Math.random() * max);
+  return randomNumb;
+}
+
+// Función manejadora
+// Evitamos el evento de recargar la página del botón
+function handleClickBtn(event) {
+  event.preventDefault();
+  changeBackground();
+  updateFace();
+}
+
+update.addEventListener('click', handleClickBtn);
+
+// Función cambiar el valor de la cara
+function updateFace() {
+  let faceSelect = face.value;
+  text.innerHTML = faceSelect;
+}
+
+// Funcion para cambiar el color
+function changeBackground() {
+  const numAle = GenerateRandomNumb(100);
+  if (numAle % 2 === 0) {
+    // Es par
+    body.classList.remove('color');
+    body.classList.add('body');
+  } else {
+    //   Si es impar
+    body.classList.add('color');
+    body.classList.remove('body');
+  }
+  console.log(numAle);
+}
