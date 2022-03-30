9ece322287895a95f481fe94e58052fe51d6926c
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-03-30 20:39:00 +0200

    tasks

--- modulo2/modulo-2-leccion-2-9-ejercicio-4-lista-de-tareas/main.js
+++ modulo2/modulo-2-leccion-2-9-ejercicio-4-lista-de-tareas/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const tasks = [
   { name: 'Recoger setas en el campo', completed: true },
   { name: 'Comprar pilas', completed: true },
@@ -8,13 +10,58 @@ const tasks = [
   },
 ];
 
-function chore() {
-  let chore = [];
-  tasks.forEach((item) => {
-    chore.push(item.name);
-  });
-  return chore;
+function renderMessage() {
+  const messageElement = document.querySelector('.message');
+
+  const totalTasks = tasks.length;
+  const completedTasks = tasks.filter((task) => task.completed).length;
+  const pendingTasks = tasks.filter((task) => !task.completed).length;
+  const message = `Tienes ${totalTasks} tareas. ${completedTasks} completadas y ${pendingTasks} por realizar.`;
+
+  messageElement.innerHTML = message;
 }
 
-const chores = chore();
-console.log(chores);
+function renderTaskList() {
+  const taskListElement = document.querySelector('.js_list');
+
+  let taskLis = '';
+
+  tasks.forEach((task) => {
+    if (task.completed) {
+      taskLis += `
+        <li class="completed js_task">
+          <input type="checkbox" name="task" checked>
+          La tarea: ${task.name}
+        </li>
+      `;
+    } else if (!task.completed) {
+      taskLis += `
+        <li class="js_task">
+          <input type="checkbox" name="task">
+          La tarea: ${task.name}
+        </li>
+      `;
+    }
+  });
+
+  taskListElement.innerHTML += taskLis;
+}
+
+function addInteraction() {
+  const liTaskList = document.querySelectorAll('.js_task');
+
+  liTaskList.forEach((liTaskElement, index) => {
+    const inputElement = liTaskElement.querySelector('input');
+
+    inputElement.addEventListener('change', (event) => {
+      const isElementChecked = event.currentTarget.checked;
+      tasks[index].completed = isElementChecked;
+      liTaskElement.classList.toggle('completed');
+      renderMessage();
+    });
+  });
+}
+
+renderMessage();
+renderTaskList();
+addInteraction();
