41267af09a3d6e827ade602d84bfc425b0fc13ba
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-03-31 16:01:39 +0200

    map method

---
+++ modulo2/modulo-2-leccion-2-10-ejercicio-2-buena-educacion/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada'];
+
+const greetNames = names.map((name) => `Bienvenida ${name}`);
+console.log(greetNames);
---
+++ modulo2/modulo-2-leccion-2-10-inflar-las-notas/main.js
@@ -0,0 +1,6 @@
+'use strict';
+const marks = [5, 4, 6, 7, 9];
+
+// Incrementamos uno a cada elemento del array
+const inflatedMarks = marks.map((mark) => mark + 1);
+console.log(inflatedMarks);
