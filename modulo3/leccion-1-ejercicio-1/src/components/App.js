7e5379a0c592eb74e2d735f77eac0474aa23f1bd
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-04-18 19:32:30 +0200

    my first page with react

--- modulo3/leccion-1-ejercicio-1/src/components/App.js
+++ modulo3/leccion-1-ejercicio-1/src/components/App.js
@@ -1,9 +1,24 @@
+// import '../styles/App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hola mundo</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// Fichero src/components/App.js
 import '../styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola mundo</h1>
+    <div>
+      <header>
+        <h1 className="title">Título de mi página</h1>
+      </header>
     </div>
   );
 }
