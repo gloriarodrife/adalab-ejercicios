74f3c31a5247888e33a5029f0a12e3b08b8b3f18
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-04-19 23:48:16 +0200

    dark mode

--- modulo3/leccion-3-2-ejercicio-3-dark-mode/src/components/App.js
+++ modulo3/leccion-3-2-ejercicio-3-dark-mode/src/components/App.js
@@ -2,25 +2,32 @@ import '../styles/App.css';
 import { useState } from 'react';
 
 function App() {
+  // hook
   const [mode, setMode] = useState('');
 
   const handleMode = (ev) => {
     const current = ev.currentTarget.parentNode;
-    setMode('dark');
-    current.classList.add('dark');
-  };
-  const handleModeLigth = (ev) => {
-    const current = ev.currentTarget.parentNode;
-    setMode('ligth');
-    current.classList.remove('dark');
-    current.classList.add('ligth');
+
+    if (mode === 'dark') {
+      setMode('ligth');
+      current.classList.remove('dark');
+      current.classList.add('ligth');
+    } else {
+      setMode('dark');
+      current.classList.add('dark');
+    }
   };
 
   return (
     <div className="App">
-      <button onClick={handleMode}>Activar dark</button>
-      <button onClick={handleModeLigth}>Desactivar dark</button>
+      <button onClick={handleMode}>Des / Activar dark</button>
+
       <p>Tienes activado el {mode}</p>
+      {/* <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero sequi
+        ex omnis impedit illum iusto necessitatibus laborum rerum fugit nostrum,
+        pariatur, inventore, assumenda eum id! Veniam dignissimos odio rem quam!
+      </p> */}
     </div>
   );
 }
