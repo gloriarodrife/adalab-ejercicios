2643433e06b48384b6ada959c088cf614e67e2d3
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-04-29 13:26:11 +0200

    update alt image

--- modulo3/ejercicio-repaso-react/src/components/Repo.js
+++ modulo3/ejercicio-repaso-react/src/components/Repo.js
@@ -6,7 +6,7 @@ const Repo = (props) => {
       {/* le paso el id al boton para poder modificar la constante de estado de los repos al darle al botón  */}
       <Delete id={props.repo.id} deleteRepo={props.deleteRepo} />
       <h2>{props.repo.full_name}</h2>
-      <img src={props.repo.image} alt="" />
+      <img src={props.repo.image} alt={props.repo.full_name} />
     </article>
   );
 };
