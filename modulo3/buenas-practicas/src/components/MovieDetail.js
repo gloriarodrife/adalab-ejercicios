d5d478554a90dd014854b45166a70da7a25e6f08
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-05-03 09:10:40 +0200

    default props with object movie

--- modulo3/buenas-practicas/src/components/MovieDetail.js
+++ modulo3/buenas-practicas/src/components/MovieDetail.js
@@ -2,18 +2,24 @@ import { useParams } from 'react-router';
 import '../styles/MovieList.scss';
 
 function MovieDetail(props) {
+  const { id } = useParams();
 
-    const { id } = useParams();
-
-    return (
-        <>
-            <li key={props.movie.id} className="movies__item movies__detail" >
-                <h2 className="movies__title">{props.movie.name}</h2>
-                <p>{props.movie.synopsis}</p>
-                <p>{props.movie.year}</p>
-            </li>
-        </>
-    );
+  return (
+    <>
+      <li key={props.movie.id} className="movies__item movies__detail">
+        <h2 className="movies__title">{props.movie.name}</h2>
+        <p>{props.movie.synopsis}</p>
+        <p>{props.movie.year}</p>
+      </li>
+    </>
+  );
 }
-
+MovieDetail.defaultProps = {
+  movie: {
+    id: 'Not defined',
+    name: 'Not defined',
+    synopsis: 'Not defined',
+    year: 'Not defined',
+  },
+};
 export default MovieDetail;
