99f8271b044dd9391a789f018ced43b0283072e8
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-05-03 21:19:32 +0200

    add input search

--- modulo3/Futurama/src/components/SpeciesFilter.js
+++ modulo3/Futurama/src/components/SpeciesFilter.js
@@ -15,6 +15,7 @@ const SpeciesFilter = (props) => {
   };
   return (
     <>
+      <input type="text" className="select_options input" />
       <label htmlFor="specie" className="select_title">
         Select a species :
       </label>
