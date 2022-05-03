78fef4e375fd7f6f839b4c401d79f8fa41ffa396
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-05-03 20:56:50 +0200

    styles detail card

--- modulo3/Futurama/src/components/Header.js
+++ modulo3/Futurama/src/components/Header.js
@@ -9,7 +9,6 @@ const Header = (props) => {
           <h1 className="header__img"> </h1>
         </Link>
         <section className="filters">
-          <input className="search" type="text" />
           <SpeciesFilter
             specieSelected={props.specieSelected}
             handleSpecieSelect={props.handleSpecieSelect}
