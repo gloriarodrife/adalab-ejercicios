27b09cf867c46298797ae45bf46511b237a569f7
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-05-03 09:13:55 +0200

    default props movie list

--- modulo3/buenas-practicas/src/components/MovieList.js
+++ modulo3/buenas-practicas/src/components/MovieList.js
@@ -2,20 +2,20 @@ import '../styles/MovieList.scss';
 import MovieItem from './MovieItem';
 
 function MovieList(props) {
+  const htmlMovies = props.moviesList.map((movieItem, index) => {
+    return <MovieItem className="movies__list" key={index} movie={movieItem} />;
+  });
 
-    const htmlMovies = props.moviesList.map((movieItem, index) => {
-        return <MovieItem className="movies__list" key={index} movie={movieItem} />
-    });
-
-    return (
-        <>
-            <ul className="movies__list">
-                {htmlMovies}
-            </ul>
-        </>
-
-
-    );
+  return (
+    <>
+      <ul className="movies__list">{htmlMovies}</ul>
+    </>
+  );
 }
+
+// Definimos que por defecto sea un listado vacio []
+MovieList.defaultProps = {
+  moviesList: [],
+};
 
 export default MovieList;
