de662e73a8e24d75650a86a9205e6dd5a0f64582
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-05-03 20:26:56 +0200

    detail character

--- modulo3/Futurama/src/components/CharacterDetail.js
+++ modulo3/Futurama/src/components/CharacterDetail.js
@@ -1,11 +1,25 @@
 import { Link } from 'react-router-dom';
 
 const CharacterDetail = (props) => {
+  const { name, image, planet, job, species, gender } = props.character;
   if (!props.character) {
     return 'loading...';
   }
 
-  return <span>{props.character.name}</span>;
+  return (
+    <>
+      <section>
+        <h2 className="card_title">{name}</h2>
+        <img className="card_img" src={image} alt={name} />
+      </section>
+      <section>
+        <p>Planet: {planet}</p>
+        <p>Job: {job}</p>
+        <p>Specie: {species}</p>
+        <p>Gender: {gender}</p>
+      </section>
+    </>
+  );
 };
 
 export default CharacterDetail;
