de0aa1a404e18f8b15172e78d3bcc36856971fab
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-05-03 09:02:55 +0200

    default props

--- modulo3/buenas-practicas/src/components/Header.js
+++ modulo3/buenas-practicas/src/components/Header.js
@@ -1,18 +1,22 @@
 import Menu from './Menu';
 
 function Header(props) {
-    return (
-        <>
-            <header className="header">
-
-                <img className="header__img" alt="movie" src="https://adalab.es/wp-content/uploads/2021/05/logo-adalab.svg" />
-                <h1 className="header__title">{props.title}</h1>
-
-            </header>
-            <Menu></Menu>
-        </>
-    );
-
+  return (
+    <>
+      <header className="header">
+        <img
+          className="header__img"
+          alt="movie"
+          src="https://adalab.es/wp-content/uploads/2021/05/logo-adalab.svg"
+        />
+        <h1 className="header__title">{props.title}</h1>
+      </header>
+      <Menu></Menu>
+    </>
+  );
 }
+Header.defaultProps = {
+  title: 'No tiene titulo ',
+};
 
 export default Header;
