f1ef7e854dfd399c59b137df45662447b89307fe
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-05-04 09:53:41 +0200

    testing

--- modulo3/leccion-3-10-testing/src/test/App.test.js
+++ modulo3/leccion-3-10-testing/src/test/App.test.js
@@ -3,6 +3,7 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import App from '../components/App';
+import Menu from '../components/Menu';
 import MovieForm from '../components/MovieForm';
 // Nombre del test y segundo una funcion
 
@@ -27,4 +28,32 @@ test('testing Label NAme', () => {
   const element = screen.getByLabelText(/Nombre/i);
   // Compruebo
   expect(element).toBeInTheDocument();
+});
+
+// Comprobar que tenemos el  enlace de Listado de pelicula
+
+test('Testing link Menu', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+
+  const element = screen.getByText(/listado de películas/i);
+  // Compruebo
+  // expect(element).toHaveAttribute('href', '/movies/list');
+
+  expect(element.href).toBe('http://localhost/movies/list');
+});
+
+// Comprobar que el códig de ls funcione correctamente
+
+test('testing Local Storage Get', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const result = localStorage.get('movies', []);
+  expect(result).toEqual([]);
 });
