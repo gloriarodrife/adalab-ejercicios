b2de9d5735b8f54c4de91c549437dcee871de0e6
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-05-09 18:40:36 +0200

    read and write file with node

---
+++ modulo4/leccion-4-1-ejercicio-1-suma/index.js
@@ -0,0 +1,62 @@
+let fs = require('fs');
+
+// const add = (a, b) => a + b;
+
+// const result = add(1, 3);
+
+// Creamos un objeto
+const myObject = {
+  user: 'Mari Carmen',
+  email: 'mari@gmail.com',
+  age: 28,
+};
+
+// Guardo el objeto en un nuevo fichero
+
+// const handleWriteFile = (err) => {
+//   if (err) {
+//     console.log('Error:', err);
+//   } else {
+//     console.log('The file has been saved!');
+//   }
+// };
+
+// fs.writeFile('./output.txt', JSON.stringify(myObject), handleWriteFile);
+// Creamos la función de callback
+//Creamos constante
+
+const readFile = (fileName, callback) => {
+  fs.readFile(fileName, 'utf-8', (err, data) => {
+    if (err) {
+      console.log(`Error:`, err);
+    } else {
+      callback(data);
+    }
+  });
+};
+
+const writeFile = (fileName, fileContent, callback) => {
+  fs.writeFile(fileName, fileContent, (err) => {
+    if (err) {
+      console.log(`Error:`, err);
+    } else {
+      callback();
+    }
+  });
+};
+
+// fs.writeFile('./input-file.json', JSON.stringify(myObject), () => {
+
+// });
+
+readFile('./input-file.json', (fileContent) => {
+  const newObject = {
+    user: 'gloriarodrife',
+    email: 'gloriarodrife@gmail.com',
+    age: 29,
+  };
+  fileContent = newObject;
+  writeFile('./input-file.json', JSON.stringify(fileContent), () => {
+    console.log('Nuevo contenido copiado correctamente');
+  });
+});
