3aecca4c4547a6bd262e6d4f2f7b2f1a8181a535
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-05-12 11:41:11 +0200

    pair class

--- modulo4/pair-netflix/src/index.js
+++ modulo4/pair-netflix/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const movies = require('./data/movies.json');
 
 // create and config server
 const server = express();
@@ -7,7 +8,17 @@ server.use(cors());
 server.use(express.json());
 
 // init express aplication
-const serverPort = 4000;
+const serverPort = 4002;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+server.get('/movies', (req, res) => {
+  console.log(req.query);
+  res.send({
+    success: true,
+    movies: movies.filter((movie) =>
+      movie.gender === '' ? movie : movie.gender === req.query.gender
+    ),
+  });
 });
